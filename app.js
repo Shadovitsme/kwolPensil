19d2ba6fe9b434faf8e2440ef1aa4d306b3e06fd
ukki <iu.cashirskaia@yandex.ru>
2025-01-06 18:05:58 +0300

    выбор функции из аякса

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 // Ваш скрипт на JavaScript
 $(document).ready(function () {
   $("form#page1").on("submit", function (e) {
-    e.preventDefault(); // предотвращаем стандартное поведение формы
+    e.preventDefault();
+    console.log($(this)[0][0].value); // предотвращаем стандартное поведение формы
     $.ajax({
       url: "save_data.php",
       type: "POST",
-      data: $(this).serialize(),
+      data: {
+        funk: "addNamePhone",
+        name: $(this)[0][0].value,
+        phone: $(this)[0][1].value,
+      },
+
       success: function (data) {
         console.log(data);
         // window.location.replace("./secondPage.html");
--- save_data.php
+++ save_data.php
@@ -1,22 +1,34 @@
 <?php
-$name = $_POST['name']; // получаем данные из формы
-$phone = $_POST['phone'];
-
 
 $databaseFile = './test.db.';  // Заменяется именем файла базы данных SQLite
-try {
-    $connection = new PDO("sqlite:$databaseFile");
-    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
-} catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
+
+
+if ($_POST['funk'] == 'addNamePhone') {
+    addNamePhone();
 }
 
-$sql = "INSERT INTO Customers (FirstName, Phone) VALUES ('$name', '$phone')";
-echo $sql;
-try {
-    $connection->exec($sql);
-    echo "Data inserted successfully";
-} catch (PDOException $e) {
-    echo "Error: " . $e->getMessage();
+
+function addNamePhone()
+{
+    global $databaseFile;
+    var_dump(($_POST));
+
+    $name = $_POST['name']; // получаем данные из формы
+    $phone = $_POST['phone'];
+    try {
+        $connection = new PDO("sqlite:$databaseFile");
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully";
+    } catch (PDOException $e) {
+        die("Connection failed: " . $e->getMessage());
+    }
+
+    $sql = "INSERT INTO Customers (FirstName, Phone) VALUES ('$name', '$phone')";
+    echo $sql;
+    try {
+        $connection->exec($sql);
+        echo "Data inserted successfully";
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
 }
