4ba6240db41787e8b7fed0c5c479d77f76e9c234
ukki <iu.cashirskaia@yandex.ru>
2025-02-03 15:05:50 +0300

    запись данных на сервак

---
+++ uploadImage.php
@@ -0,0 +1,15 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+var_dump($_FILES);
+
+$uploadDir = '/home/ukki/Documents/bigPensil/kwolkarandash/clientsRefs/';
+$uploadFile = $uploadDir . basename($_FILES['photo']['name']);
+
+if (move_uploaded_file($_FILES['photo']['tmp_name'], $uploadFile)) {
+    echo json_encode(['status' => 'success', 'message' => 'File uploaded successfully.']);
+} else {
+    echo json_encode(['status' => 'error', 'message' => 'Failed to move uploaded file.']);
+}
