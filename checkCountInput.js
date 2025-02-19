be5e5fc43c843a202cdbe4cdd8d47105a5c10f7c
ukki <iu.cashirskaia@yandex.ru>
2025-02-19 11:02:17 +0300

    правки из отаска - теперь в инпут считалки с клавы нельзя ввести что угодно - теперь радиоботон с желтой точкой

--- checkCountInput.js
+++ checkCountInput.js
@@ -1,7 +1,9 @@
 function checkCountInput(event) {
   if (event.target.value > 5) {
     event.target.value = 5;
-  } else if (e.target.value < 0) {
+  } else if (event.target.value < 0) {
     event.target.value = 0;
+  } else if (event.target.value == "" || event.target.value == undefined) {
+    event.target.value=0
   }
 }
