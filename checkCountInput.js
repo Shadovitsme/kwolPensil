42e6cd0ce82b68a73b228d3d63ef7a7e479ebff3
ukki <iu.cashirskaia@yandex.ru>
2025-02-27 15:28:55 +0300

    исправление части проблем из отаска

--- checkCountInput.js
+++ checkCountInput.js
@@ -1,9 +1,14 @@
 function checkCountInput(event) {
   if (event.target.value > 5) {
     event.target.value = 5;
+  } else if (
+    event.target.value.startsWith("0") &&
+    event.target.value.length > 1
+  ) {
+    event.target.value = event.target.value.slice(1);
   } else if (event.target.value < 0) {
     event.target.value = 0;
   } else if (event.target.value == "" || event.target.value == undefined) {
-    event.target.value=0
+    event.target.value = 0;
   }
 }
